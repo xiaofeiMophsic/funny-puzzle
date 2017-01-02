16026a2a9b6ebb168595adef66ff009e8571d510
mophsic <1084549765@qq.com>
2017-01-02 18:04:22 +0800

    使用循环的方式求解斐波那契数列

---
+++ src/com/mophsic/loop/Fibonacci.java
@@ -0,0 +1,8 @@
+package com.mophsic.loop;
+
+/**
+ * @author xiaofei
+ * @date 2017/1/2
+ */
+public class Fibonacci {
+}
---
+++ src/com/mophsic/loop/FibonacciTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author xiaofei
+ * @date 2017/1/2
+ */
+public class FibonacciTest {
+
+}
