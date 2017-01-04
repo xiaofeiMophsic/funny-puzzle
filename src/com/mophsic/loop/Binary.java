ebdc4ef1be5f8a4afbda1fd91b603391e1523118
mophsic <1084549765@qq.com>
2017-01-04 08:15:26 +0800

    求解二进制中1的个数

---
+++ src/com/mophsic/loop/Binary.java
@@ -0,0 +1,40 @@
+package com.mophsic.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 求整数二进制中1的个数
+ * @author xiaofei
+ * @date 2017/1/4
+ */
+public class Binary {
+
+    public int count(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int count = 0;
+
+        do {
+            count ++;
+        } while (0 != (n = (n & n - 1)));
+
+        return count;
+    }
+    @Test
+    public void test(){
+        // 功能测试
+        int c = count(15);
+        Assert.assertEquals(4, c);
+
+        // 边界条件
+        int c1 = count(0);
+        Assert.assertEquals(0, c1);
+
+        // 负面测试
+        int c2 = count(Integer.MAX_VALUE);
+        Assert.assertEquals(31, c2);
+    }
+}
